179c2aaba95d7e175063ae29c2404c6900991b14
dennis199441 <wcgnuehc@yahoo.com.hk>
2019-12-16 21:58:10 +0800

    add tf.summary

--- gans.py
+++ gans.py
@@ -110,7 +110,6 @@ def generate_and_save_images(model, epoch, test_input):
         plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
         plt.axis('off')
     plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
-    plt.close()
 
 # Display a single image using the epoch number
 def display_image(epoch_no):
