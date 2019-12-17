99043a258d9891324e393330f89c4a80c7ce77d7
dennis199441 <wcgnuehc@yahoo.com.hk>
2019-12-17 15:53:08 +0800

    Update pokeGAN.py

--- pokeGAN.py
+++ pokeGAN.py
@@ -119,7 +119,7 @@ def generate_and_save_images(model, epoch, test_input):
     fig = plt.figure(figsize=(4,4))
     for i in range(predictions.shape[0]):
         plt.subplot(4, 4, i+1)
-        plt.imshow(predictions[i, :, :, 3])
+        plt.imshow(predictions[i, :, :, :])
         plt.axis('off')
     plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
     plt.close()
