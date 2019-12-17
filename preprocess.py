9d041633b09fdd6ca93640fc562ed511511d47ed
dennis199441 <wcgnuehc@yahoo.com.hk>
2019-12-17 11:45:03 +0800

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -2,16 +2,23 @@ from PIL import Image
 import glob, os
 import numpy as np
 
-if not os.path.exists('../preprocessed'):
-	os.mkdir("../preprocessed/")
+output_folder = "../preprocessed"
+if not os.path.exists(output_folder):
+	os.mkdir(output_folder)
 
 folder_name = "../pokemon"
 filenames = glob.glob(folder_name + '/*.png')
 
 for i,filename in enumerate(filenames):
     img = Image.open(filename)
-    new_filename = filename.replace(folder_name, "../preprocessed/")
-    resized_img = img.resize((96,96))
+    new_filename = filename.replace(folder_name, output_folder)
+    resized_img = img.resize((96,96, 3))
     resized_img.save(new_filename, "png")
-    np_array = np.asarray(resized_img)
-    print("new_filename = {}, shape = {}".format(new_filename, np_array.shape))
+
+print("process done!")
+
+filenames = glob.glob(output_folder + '/*.png')
+for i,filename in enumerate(filenames):
+    img = Image.open(filename)
+    array = np.asarray(img)
+    print("filename = {}, shape = {}".format(filename, array.shape))
